0a6bc8e042940ab9825180f6d47b403b94fa54c0
Jan Marek <mail@janmarek.net>
2020-10-27 09:33:12 +0100

    jsx expressions

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
+    const a = 123;
+    const classNameX = "abcd";
+
     return (
         <div className="App">
-            <div className="cls">123</div>
+            <div className={classNameX}>{a + 1}</div>
             {React.createElement("div", {className: "cls"}, "123")}
         </div>
     );
