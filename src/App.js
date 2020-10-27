5c78631d3ad5f585e9f4f3f6ff7c2754136c31b8
Jan Marek <mail@janmarek.net>
2020-10-27 09:31:35 +0100

    jsx example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <div className="cls">123</div>
+            {React.createElement("div", {className: "cls"}, "123")}
+        </div>
+    );
 }
 
 export default App;
