eba215b36bff1745ae9b2c9ce3d3614058f945f2
Jan Marek <mail@janmarek.net>
2020-10-27 11:48:55 +0100

    uncontrolled component form

--- src/AddPage.js
+++ src/AddPage.js
@@ -1,3 +1,35 @@
 export function AddPage() {
-    return <h2>Add Contact</h2>;
+    function submit(e) {
+        e.preventDefault();
+        const nameValue = e.target.name.value;
+        const phoneValue = e.target.phone.value;
+        const emailValue = e.target.email.value;
+        console.log(nameValue, phoneValue, emailValue);
+    }
+
+    return (
+        <>
+            <h2>Add Contact</h2>
+            <form onSubmit={submit}>
+                <p>
+                    Name:
+                    <br />
+                    <input type="text" name="name" />
+                </p>
+                <p>
+                    Phone:
+                    <br />
+                    <input type="text" name="phone" />
+                </p>
+                <p>
+                    Email:
+                    <br />
+                    <input type="text" name="email" />
+                </p>
+                <p>
+                    <button type="submit">Save</button>
+                </p>
+            </form>
+        </>
+    );
 }
