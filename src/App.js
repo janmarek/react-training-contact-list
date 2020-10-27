6797a56a5b81981b73396efec920ed409fbb99c8
Jan Marek <mail@janmarek.net>
2020-10-27 09:36:46 +0100

    contact component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ function App() {
 
     return (
         <div className="App">
+            <Contact name="Honza" phone={a} email="mail@google.com" />
             <div className={classNameX}>{a + 1}</div>
             {React.createElement("div", {className: "cls"}, "123")}
+        </div>
+    );
+}
+
+function Contact(props) {
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <p>phone: {props.phone}</p>
+            <p>email: {props.email}</p>
         </div>
     );
 }
