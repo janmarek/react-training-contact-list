9d23914291c33b5d84a83287308a05bf3ec0eea6
Jan Marek <mail@janmarek.net>
2020-10-27 11:37:11 +0100

    router switch

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import {Container} from "react-bootstrap";
 import {ContactListPage} from "./ContactListPage";
 import {AddPage} from "./AddPage";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
     return (
         <Container>
             <BrowserRouter>
-                <Route path="/add">
-                    <AddPage />
-                </Route>
-                <Route exact path="/">
-                    <ContactListPage />
-                </Route>
+                <Switch>
+                    <Route path="/add">
+                        <AddPage />
+                    </Route>
+                    <Route path="/">
+                        <ContactListPage />
+                    </Route>
+                </Switch>
             </BrowserRouter>
         </Container>
     );
