fa8b02b698a871da99d82f9d4fb48af193a8d8a4
Jan Marek <mail@janmarek.net>
2020-10-27 11:14:07 +0100

    styled components

--- src/Score.js
+++ src/Score.js
@@ -1,14 +1,24 @@
 import {Button} from "react-bootstrap";
-import "./Score.scss";
+import styled from "styled-components";
 
 export function Score(props) {
     return (
-        <div className="score">
-            Score: {props.domaci}:{props.hoste}
+        <ScoreDiv>
+            <p className="red">
+                Score: {props.domaci}:{props.hoste}
+            </p>
             <p>
                 <Button onClick={props.increaseDomaci}>Domaci</Button>{" "}
                 <Button onClick={props.increaseHoste}>Hoste</Button>
             </p>
-        </div>
+        </ScoreDiv>
     );
 }
+
+const ScoreDiv = styled.div`
+    background: #ccc;
+    padding: 1em;
+    p {
+        margin-top: 1em;
+    }
+`;
